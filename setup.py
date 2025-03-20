5c1b3f9dcc78fe96089ae2b2069c36ab8f0a9044
Jigar vyas <80967728+jigarvyasidea@users.noreply.github.com>
2025-03-20 17:42:04 +0530

    folder strucutre ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='manekTECH Chatbot',
+    version='0.0.0',
+    author='Jigar vyas',
+    author_email='jigarvyasidea@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
